78671eb30424eebf52dba50201514cc313edd0bc
nora <nora7373@mail.ru>
2023-10-29 17:31:22 +0300

    second commit

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -14,6 +14,7 @@ public class SearchTests {
     }
     @Test
     void successfulSearchTest() {
+
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://ru.selenide.org"));
