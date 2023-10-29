744f3693aaa981f79395371d0fd16118fb5402f5
Nora7373 <97349075+Nora7373@users.noreply.github.com>
2023-10-29 17:28:44 +0300

    Update SearchTests.java

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -14,6 +14,7 @@ public class SearchTests {
     }
     @Test
     void successfulSearchTest() {
+        // fgfhgfhgfhfhgfgh
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://ru.selenide.org"));
